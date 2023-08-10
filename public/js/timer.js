d71fecdeceff9651fd93ae5a46ae5eded4186707
Ignacio Serdá <ignserda@gmail.com>
2023-08-10 20:49:01 -0300

    juego terminado

---
+++ public/js/timer.js
@@ -0,0 +1,18 @@
+const container = document.getElementById("timer-container");
+const miniContainer = document.getElementById("timer");
+let number = 30;
+
+
+let timer  = setInterval(() => {
+    miniContainer.remove()
+    container.innerHTML = `
+        <div id="timer" class="count-decoration">
+            <p>${number}</p>
+        </div>      
+    `
+    number --;
+    if (number == -1){
+        clearInterval(timer);
+        fetchTimer();
+    }
+}, 1000);
